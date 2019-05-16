c323806f9c36c6c88ab196a8588bbfe46acd7fe8
Chee Sing Lee <lee@bigml.com>
2019-05-16 13:51:28 -0700

    more inputs for unit tests

--- tests/test_bioenergetics.py
+++ tests/test_bioenergetics.py
@@ -21,31 +21,25 @@ daylengths = {'March': 11.83, 'April': 13.4, 'May': 14.73, 'June': 15.42,
 def daphnia_data():
     total_daphnia = 5020.65
     daphnia_size = 1.26
-    x, y = util.select_rows('tests/data/daphnia-vd.csv', site, month, year)
+    x, y = util.select_rows('tests/data/daphnia-vd.csv',
+                            'depth', 'total daphnia',
+                            site, month, year)
     return DaphniaData(total_daphnia, x, y, size=daphnia_size)
 
 
 @pytest.fixture
 def bathymetry():
-    elev = []
-    area = []
-    with open('tests/data/fall-creek-bath.csv') as fid:
-        reader = DictReader(fid)
-        for row in reader:
-            elev.append(int(row['elevation (m)']))
-            area.append(float(row['2d_area (m2)']))
+    elev, area = util.select_rows('tests/data/bathymetry.csv',
+                                  'elevation (m)', '2d_area (m2)',
+                                  site=site)
     return InterpolatedFunction(elev, area)
 
 
 @pytest.fixture
 def temperature():
-    with open('tests/data/fall-creek-temp-june-2015.csv') as fid:
-        reader = DictReader(fid)
-        temperatures = []
-        depths = []
-        for row in reader:
-            temperatures.append(float(row['temp']))
-            depths.append(float(row['depth']))
+    depths, temperatures = util.select_rows('tests/data/temperatures.csv',
+                                            'depth', 'temperature',
+                                            site, month, year)
     return InterpolatedFunction(depths, temperatures)
 
 
@@ -62,7 +56,6 @@ def truncate(results, decimals=3):
 def test_prey_depth_profile(daphnia_data):
     xs = np.arange(daphnia_data.min_depth, daphnia_data.max_depth, 0.5)
     ys = list(map(daphnia_data.prey_count, xs))
-    print(ys)
     auc = trapz(ys, xs)
     assert auc == 5020.65
     assert (daphnia_data.prey_count(10) <
@@ -76,3 +69,11 @@ def test_batch(temperature, bathymetry, daphnia_data, regression_artifact):
                   daylengths[month], light_extinction)
     results = truncate(batch.run()[0])
     assert results == regression_artifact
+
+
+def test_no_dvm(temperature, bathymetry, daphnia_data):
+    starting_mass = 3.0
+    batch = Model(starting_mass, daphnia_data, temperature, bathymetry,
+                  daylengths[month], light_extinction, allow_dvm=False)
+    results = truncate(batch.run()[0])
+    print(results)
