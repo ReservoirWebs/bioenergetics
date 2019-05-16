0e04b7a5725e0165a63b4d65dcfe603892f23819
Chee Sing Lee <lee@bigml.com>
2019-05-16 13:51:42 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ setup(
     name="BioEnergetics",
     version="0.1",
     packages=find_packages(),
+
+    install_requires=['numpy', 'scipy', 'matplotlib'],
+    author='Chee Sing Lee',
+    license='GPLv2'
 )
